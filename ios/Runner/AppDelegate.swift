d0a7a8449ad9271c5f18ecf2d026dc6143ab5757
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-06-18 16:22:46 +0900

    環境変数の読み込み

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import flutter_config
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +10,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey(FlutterConfigPlugin.env(for: "GOOGLE_MAPS_API_KEY_FOR_IOS"))
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
