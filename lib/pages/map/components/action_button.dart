275644bd5c217b8856cee6874d0d15c1e35d159b
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-07-15 21:36:20 +0900

    マップページコンポーネントの作成

---
+++ lib/pages/map/components/action_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({
+    super.key,
+    required this.onPressed,
+    required this.icon,
+    required this.isOpened,
+  });
+  final void Function() onPressed;
+  final String icon;
+  final bool isOpened;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      elevation: isOpened ? 5 : 0,
+      onPressed: onPressed,
+      child: SvgPicture.asset(
+        icon,
+        width: 30,
+        height: 30,
+      ),
+    );
+  }
+}
---
+++ lib/pages/map/components/map_components.dart
@@ -0,0 +1,3 @@
+export 'action_button.dart';
+export 'select_map_type_modal.dart';
+export 'tab_actions_button.dart';
