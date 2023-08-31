eb6c4b04203258ffd84e08d79900f6ec11cbb750
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-08-31 19:07:57 +0900

    スタイルの共通化

--- lib/utils/styles/text.dart
+++ lib/utils/styles/text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:search_roof_top_app/utils/utils.dart';
 
 class AppTextStyle {
   AppTextStyle._();
@@ -11,5 +12,14 @@ class AppTextStyle {
   static const mapTypeModalOption = TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.w600,
+  );
+
+  static const createMarkerTextFieldLabel = TextStyle(
+    fontSize: 16,
+    color: ColorName.black,
+  );
+
+  static const saveMarkerText = TextStyle(
+    fontSize: 16,
   );
 }
