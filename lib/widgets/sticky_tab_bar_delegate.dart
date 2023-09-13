652003207be3efd58e8d42cf49519035e9a2290f
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-09-13 22:14:09 +0900

    ウィジェットを追加

---
+++ lib/widgets/sticky_tab_bar_delegate.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:search_roof_top_app/utils/utils.dart';
+
+class StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  const StickyTabBarDelegate(this.tabBar);
+
+  final TabBar tabBar;
+
+  @override
+  double get minExtent => tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: ColorName.lightGrey,
+            width: 2,
+          ),
+        ),
+      ),
+      child: tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(StickyTabBarDelegate oldDelegate) {
+    return tabBar != oldDelegate.tabBar;
+  }
+}
