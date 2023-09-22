c0791ae5f6d1723b8f59085812033d42bb19a4e2
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-09-22 12:03:25 +0900

    全体コンポーネントの追加

---
+++ lib/widgets/error_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_roof_top_app/pages/auth/sign_in_page.dart';
+import 'package:search_roof_top_app/utils/utils.dart';
+import 'package:search_roof_top_app/widgets/widgets.dart';
+
+class ErrorPage extends HookConsumerWidget {
+  const ErrorPage({
+    super.key,
+    required this.onTapReload,
+    this.onTapOk,
+    this.error,
+    this.showModal = true,
+  });
+
+  final void Function() onTapReload;
+  final void Function(BuildContext)? onTapOk;
+  final Object? error;
+  final bool showModal;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const title = '読み込みに失敗しました。';
+    const message = '通信環境のいい場所で\n再度読み込みをお試しください。';
+    const cancelLabel = '再読み込み';
+
+    final dialog = showDialog<void>(
+      context: context,
+      builder: (context) => OkCancelAlertDialog(
+        title: title,
+        message: message,
+        cancelLabel: cancelLabel,
+        okLabel: 'OK',
+        onTapCancel: (context) {
+          Navigator.pop(context);
+          onTapReload();
+        },
+        onTapOk: onTapOk ?? Navigator.pop,
+      ),
+    );
+
+    useEffectOnce(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        if (error != null && error is AppException) {
+          final appException = error! as AppException;
+
+          if (appException.code == unauthorized) {
+            await ref
+                .read(sharedPreferencesServiceProvider)
+                .deleteAuthCredentials();
+            ref
+                .read(scaffoldMessengerServiceProvider)
+                .showExceptionSnackBar(generalUnauthorizedMessage);
+            await ref
+                .read(navigatorKeyProvider)
+                .currentState
+                ?.pushAndRemoveUntil(
+                  SignInPage.route(),
+                  (_) => false,
+                );
+          }
+        }
+
+        if (showModal) {
+          await dialog;
+        }
+      });
+
+      return null;
+    });
+
+    return showModal
+        ? const SizedBox()
+        : Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const Gap(20),
+                const Text(
+                  message,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: ColorName.deepGrey,
+                    fontSize: 16,
+                  ),
+                ),
+                const Gap(20),
+                TextButton(
+                  onPressed: onTapReload,
+                  child: const Text(
+                    cancelLabel,
+                    style: TextStyle(
+                      color: ColorName.amber,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+  }
+}
+
+class ScaffoldErrorPage extends StatelessWidget {
+  const ScaffoldErrorPage({
+    super.key,
+    required this.onTapReload,
+    this.onTapOk,
+    this.error,
+    this.showModal = true,
+  });
+
+  final void Function() onTapReload;
+  final void Function(BuildContext)? onTapOk;
+  final Object? error;
+  final bool showModal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ErrorPage(
+        onTapReload: onTapReload,
+        onTapOk: onTapOk,
+        error: error,
+        showModal: showModal,
+      ),
+    );
+  }
+}
---
+++ lib/widgets/ok_cancel_alert_dialog.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_roof_top_app/utils/utils.dart';
+
+class OkCancelAlertDialog extends HookConsumerWidget {
+  const OkCancelAlertDialog({
+    super.key,
+    required this.title,
+    this.message,
+    required this.cancelLabel,
+    required this.okLabel,
+    required this.onTapCancel,
+    required this.onTapOk,
+  });
+
+  final String title;
+  final String? message;
+  final String cancelLabel;
+  final String okLabel;
+  final void Function(BuildContext) onTapCancel;
+  final void Function(BuildContext) onTapOk;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AlertDialog(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+      ),
+      content: message != null
+          ? Text(
+              message!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: ColorName.deepGrey,
+                fontSize: 16,
+              ),
+            )
+          : null,
+      actionsAlignment: MainAxisAlignment.spaceAround,
+      actionsPadding: EdgeInsets.zero,
+      actions: <Widget>[
+        const Divider(
+          height: 1,
+          thickness: 1,
+          color: ColorName.lightGrey,
+        ),
+        Row(
+          children: [
+            _buildAction(
+              label: cancelLabel,
+              onTap: onTapCancel,
+              // context: context,
+            ),
+            const SizedBox(
+              height: 50,
+              child: VerticalDivider(
+                width: 1,
+                thickness: 1,
+                color: ColorName.lightGrey,
+              ),
+            ),
+            _buildAction(
+              label: okLabel,
+              onTap: onTapOk,
+              // context: context,
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _buildAction({
+    required String label,
+    required void Function(BuildContext) onTap,
+  }) {
+    final context = useContext();
+    return Expanded(
+      child: InkWell(
+        onTap: () => onTap(context),
+        child: SizedBox(
+          height: 50,
+          child: Center(
+            child: Text(
+              label,
+              style: const TextStyle(
+                color: ColorName.amber,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
