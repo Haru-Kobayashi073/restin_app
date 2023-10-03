8f832e9e33a2254e0e43b3649c5de66c63592e38
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-03 20:28:42 +0900

    fileリポジトリのリファクタリング

--- lib/repositories/file/file_repository_impl.dart
+++ lib/repositories/file/file_repository_impl.dart
@@ -2,11 +2,11 @@ import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:search_roof_top_app/repositories/file/file_repository.dart';
 import 'package:search_roof_top_app/utils/utils.dart';
+import 'package:tuple/tuple.dart';
 
 final fileRepositoryImplProvider = Provider<FileRepository>(
   (ref) => FileRepositoryImpl(
@@ -22,35 +22,27 @@ class FileRepositoryImpl implements FileRepository {
   );
   final FirebaseAuth _auth;
   final FirebaseStorage _storage;
-  static const fileName = 'profile_icon_image.jpg';
 
   @override
   User? get currentUser => _auth.currentUser;
 
   @override
-  Future<File?> pickImageAndUpload() async {
+  Future<Tuple2<String, File>> pickImageAndUpload() async {
     final picker = ImagePicker();
+    final fileName = returnJpgFileName();
     final uid = currentUser!.uid;
     final image = await picker.pickImage(source: ImageSource.gallery);
     final file = File(image!.path);
-    final storageRef = _storage.ref().child('users/$uid/$fileName');
+    final storageRef = _storage.ref().child('users').child(uid).child(fileName);
     await storageRef.putFile(file);
-    return file;
+    final imageUrl = await storageRef.getDownloadURL();
+    return Tuple2(imageUrl, file);
   }
 
   @override
-  Future<Image> getDownloadUrl() async {
+  Future<void> deleteFile(String url) async {
     final uid = currentUser!.uid;
-
-    final reference = _storage.ref().child('users/$uid/$fileName');
-    final imageUrl = await reference.getDownloadURL();
-    return Image.network(imageUrl);
-  }
-
-  @override
-  Future<void> deleteFile() async {
-    final uid = currentUser!.uid;
-    final reference = _storage.ref().child('users/$uid/$fileName');
+    final reference = _storage.ref().child('users').child(uid).child(url);
     await reference.delete();
   }
 }
