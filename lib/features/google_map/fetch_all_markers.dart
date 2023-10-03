18bd379719b0505518ae0e6daf2195fa7babb7fc
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-03 20:08:09 +0900

    SearchResultCardで受け取るデータをMarkerDataに変更

--- lib/features/google_map/fetch_all_markers.dart
+++ lib/features/google_map/fetch_all_markers.dart
@@ -1,15 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_roof_top_app/models/marker_data.dart';
+import 'package:search_roof_top_app/pages/map/marker_detail_modal.dart';
 import 'package:search_roof_top_app/repositories/marker/marker_repository_impl.dart';
 import 'package:search_roof_top_app/utils/utils.dart';
 
-final fetchAllMarkersProvider = StreamProvider.autoDispose<List<Marker>>(
-  (ref) async* {
+final showModalProvider = StateProvider.autoDispose<
+    Future<void> Function({
+      required BuildContext context,
+      required MarkerData markerData,
+    })>(
+  (ref) => ({
+    required context,
+    required markerData,
+  }) async {
+    await showModalBottomSheet<void>(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      context: context,
+      elevation: 0,
+      builder: (context) {
+        return MarkerDetailModal(
+          markerData: markerData,
+        );
+      },
+    );
+  },
+);
+
+final fetchAllMarkersProvider =
+    StreamProvider.autoDispose.family<List<Marker>, BuildContext>(
+  (ref, context) async* {
     final read = ref.read;
     final isNetworkCheck = await isNetworkConnected();
     try {
-      final response = read(markerRepositoryImplProvider).fetchAllMarkers();
+      final response =
+          read(markerRepositoryImplProvider).fetchAllMarkers().map((event) {
+        final list = <Marker>[];
+        for (final document in event) {
+          list.add(
+            Marker(
+              markerId: MarkerId(document.markerId),
+              position: LatLng(
+                document.latitude,
+                document.longitude,
+              ),
+              infoWindow: InfoWindow(
+                title: document.title,
+                snippet: document.description,
+              ),
+              consumeTapEvents: true,
+              onTap: () {
+                ref.read(showModalProvider).call(
+                      context: context,
+                      markerData: document,
+                    );
+              },
+            ),
+          );
+        }
+        return list;
+      });
+      debugPrint('全マーカーを取得しました。');
+      yield* response.asBroadcastStream();
+    } on AppException catch (e) {
+      if (!isNetworkCheck) {
+        const exception = AppException(
+          message: 'Maybe your network is disconnected. Please check yours.',
+        );
+        throw exception;
+      }
+
+      debugPrint('全マーカーの取得エラー: $e');
+      rethrow;
+    }
+  },
+);
+
+final fetchAllMarkerDataProvider =
+    StreamProvider.autoDispose.family<List<MarkerData>, BuildContext>(
+  (ref, context) async* {
+    final read = ref.read;
+    final isNetworkCheck = await isNetworkConnected();
+    try {
+      final response =
+          read(markerRepositoryImplProvider).fetchAllMarkers().map((event) {
+        final list = <MarkerData>[];
+        event.forEach(list.add);
+        return list;
+      });
       debugPrint('全マーカーを取得しました。');
       yield* response.asBroadcastStream();
     } on AppException catch (e) {
--- lib/features/google_map/search_markers.dart
+++ lib/features/google_map/search_markers.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_roof_top_app/features/google_map/google_map.dart';
+import 'package:search_roof_top_app/models/marker_data.dart';
 import 'package:search_roof_top_app/repositories/marker/marker_repository_impl.dart';
 import 'package:search_roof_top_app/utils/utils.dart';
+import 'package:tuple/tuple.dart';
 
-final searchMarkersProvider =
-    FutureProvider.autoDispose.family<List<Marker>?, String>(
-  (ref, query) async {
+final searchMarkersProvider = FutureProvider.autoDispose
+    .family<List<Marker>?, Tuple2<String, BuildContext>>(
+  (ref, param) async {
     final read = ref.read;
     final isNetworkCheck = await isNetworkConnected();
     try {
-      final response =
-          read(markerRepositoryImplProvider).searchMarkers(query: query);
+      final list = <Marker>[];
+      await read(markerRepositoryImplProvider)
+          .searchMarkers(query: param.item1)
+          .then((value) {
+        for (final document in value) {
+          list.add(
+            Marker(
+              markerId: MarkerId(document.markerId),
+              position: LatLng(
+                document.latitude,
+                document.longitude,
+              ),
+              infoWindow: InfoWindow(
+                title: document.title,
+                snippet: document.description,
+              ),
+              consumeTapEvents: true,
+              onTap: () {
+                ref.read(showModalProvider).call(
+                      context: param.item2,
+                      markerData: document,
+                    );
+              },
+            ),
+          );
+        }
+        return list;
+      });
+      debugPrint('全マーカーを取得しました。');
+      return list;
+    } on AppException catch (e) {
+      if (!isNetworkCheck) {
+        const exception = AppException(
+          message: 'Maybe your network is disconnected. Please check yours.',
+        );
+        throw exception;
+      }
+
+      debugPrint('全マーカーの取得エラー: $e');
+      rethrow;
+    }
+  },
+);
+
+final searchMarkerDataProvider = FutureProvider.autoDispose
+    .family<List<MarkerData>?, Tuple2<String, BuildContext>>(
+  (ref, param) async {
+    final read = ref.read;
+    final isNetworkCheck = await isNetworkConnected();
+    try {
+      final response = await read(markerRepositoryImplProvider)
+          .searchMarkers(query: param.item1);
       debugPrint('全マーカーを取得しました。');
       return response;
     } on AppException catch (e) {
