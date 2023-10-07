ffc4d6f0b800c3aa42f57453705daf04b6a7a837
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-07 21:23:57 +0900

    保存済みの全マーカーを取得するproviderを作成

---
+++ lib/features/user/fetch_book_mark_markers.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_roof_top_app/models/marker_data.dart';
+import 'package:search_roof_top_app/repositories/user/user_repository_impl.dart';
+import 'package:search_roof_top_app/utils/utils.dart';
+
+final fetchBookMarkMarkersProvider =
+    FutureProvider.autoDispose<List<MarkerData>>(
+  (ref) async {
+    final read = ref.read;
+    final isNetworkCheck = await isNetworkConnected();
+    final list = <MarkerData>[];
+    try {
+      await read(userRepositoryImplProvider)
+          .fetchUserBookMarkMarkers()
+          .then((value) => value!.forEach(list.add));
+      debugPrint('保存済みの全マーカーを取得しました。');
+      return list;
+    } on AppException catch (e) {
+      if (!isNetworkCheck) {
+        const exception = AppException(
+          message: 'Maybe your network is disconnected. Please check yours.',
+        );
+        throw exception;
+      }
+
+      debugPrint('保存済みの全マーカーの取得エラー: $e');
+      rethrow;
+    }
+  },
+);
