cea9f2ee2fc9fd1d4ef90b5f21feb1d68464821a
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-12 01:26:55 +0900

    ユーザー情報取得メソッドをuidを受け取れるように変更

--- lib/features/user/fetch_user_data.dart
+++ lib/features/user/fetch_user_data.dart
@@ -4,22 +4,24 @@ import 'package:search_roof_top_app/models/user_data.dart';
 import 'package:search_roof_top_app/repositories/user/user_repository_impl.dart';
 import 'package:search_roof_top_app/utils/utils.dart';
 
-final fetchUserDataProvider = FutureProvider.autoDispose<UserData>(
-    (ref) async {
-      final read = ref.read;
-      final isNetworkCheck = await isNetworkConnected();
-      try {
-        final response = await read(userRepositoryImplProvider).fetchUserData();
-        return response;
-      } on AppException catch (e) {
-        if (!isNetworkCheck) {
-          const exception = AppException(
-            message: 'Maybe your network is disconnected. Please check yours.',
-          );
-          throw exception;
-        }
-        debugPrint('ユーザー情報取得エラー: $e');
-        rethrow;
+final fetchUserDataProvider =
+    FutureProvider.autoDispose.family<UserData, String?>(
+  (ref, uid) async {
+    final read = ref.read;
+    final isNetworkCheck = await isNetworkConnected();
+    try {
+      final response =
+          await read(userRepositoryImplProvider).fetchUserData(uid);
+      return response;
+    } on AppException catch (e) {
+      if (!isNetworkCheck) {
+        const exception = AppException(
+          message: 'Maybe your network is disconnected. Please check yours.',
+        );
+        throw exception;
       }
-    },
-  );
+      debugPrint('ユーザー情報取得エラー: $e');
+      rethrow;
+    }
+  },
+);
