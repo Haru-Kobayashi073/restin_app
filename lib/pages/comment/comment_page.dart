afeab514c57dc9583ab21ae2dd96e765e080db96
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-17 12:41:13 +0900

    リファクタリング、レイアウト修正

--- lib/pages/comment/comment_page.dart
+++ lib/pages/comment/comment_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:search_roof_top_app/features/google_map/google_map.dart';
 import 'package:search_roof_top_app/pages/comment/components/comment_components.dart';
@@ -20,15 +19,14 @@ class CommentPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final commentController = useTextEditingController();
-
     return SizedBox(
       height: context.deviceHeight - context.deviceHeight * 0.1,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
             margin: const EdgeInsets.all(8),
-            alignment: Alignment.centerLeft,
+            alignment: Alignment.topLeft,
             child: IconButton(
               onPressed: () => Navigator.pop(context),
               icon: const Icon(Icons.close),
@@ -59,56 +57,9 @@ class CommentPage extends HookConsumerWidget {
                 ),
                 loading: () => const Loading(),
               ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 32, 8, 32),
-                  child: CommonTextField(
-                    controller: commentController,
-                    keyboardType: TextInputType.multiline,
-                    labelText: 'コメントを投稿する',
-                  ),
-                ),
-              ),
-              IconButton(
-                onPressed: () async {
-                  await createMarkerComment(
-                    markerId: markerId,
-                    comment: commentController.text,
-                    context: context,
-                    ref: ref,
-                  );
-                  commentController.clear();
-                },
-                padding: const EdgeInsets.only(right: 8),
-                icon: const Icon(Icons.send),
-              ),
-            ],
-          ),
-          const SizedBox(height: 32),
+          CommentButton(markerId: markerId),
         ],
       ),
     );
-  }
-
-  Future<void> createMarkerComment({
-    required String markerId,
-    required String comment,
-    required BuildContext context,
-    required WidgetRef ref,
-  }) async {
-    await ref.read(createMarkerCommentProvider).call(
-          markerId: markerId,
-          comment: comment,
-          onSuccess: () {
-            ScaffoldMessengerService.showSuccessSnackBar(
-              context,
-              'コメントを投稿しました',
-            );
-            ref.invalidate(fetchMarkersCommentsProvider(markerId));
-            
-          },
-        );
   }
 }
