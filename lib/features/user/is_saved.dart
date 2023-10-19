28cc9955a4c5f75354732be2cf0692e9074ed356
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-19 11:55:22 +0900

    認証済みでない場合、保存表示にしないように修正

--- lib/features/user/is_saved.dart
+++ lib/features/user/is_saved.dart
@@ -1,10 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_roof_top_app/features/auth/auth.dart';
 import 'package:search_roof_top_app/models/marker_data.dart';
 import 'package:search_roof_top_app/repositories/user/user_repository_impl.dart';
 
 final isSavedProvider =
     StateProvider.family<bool, MarkerData>((ref, markerData) {
-  final uid = ref.watch(userRepositoryImplProvider).currentUser!.uid;
-  final isSaved = markerData.bookMarkedUserIds?.contains(uid) ?? false;
-  return isSaved;
+  final isAuthenticated = ref.read(isAuthenticatedProvider);
+  try {
+    if (!isAuthenticated) {
+      return false;
+    }
+    final uid = ref.watch(userRepositoryImplProvider).currentUser!.uid;
+    final isSaved = markerData.bookMarkedUserIds?.contains(uid) ?? false;
+    return isSaved;
+  } on Exception catch (e) {
+    debugPrint(e.toString());
+    rethrow;
+  }
 });
