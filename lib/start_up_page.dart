388f322fe6c762fb5361e6444b1177846fc5d41e
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-19 11:59:33 +0900

    初期ローンチやスキップ済みかを監視し、初期表示画面を対応させる

--- lib/start_up_page.dart
+++ lib/start_up_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:search_roof_top_app/pages/auth/sign_in_page.dart';
 import 'package:search_roof_top_app/pages/home/main_page.dart';
+import 'package:search_roof_top_app/utils/utils.dart';
 import 'package:search_roof_top_app/widgets/widgets.dart';
 
 import 'repositories/auth/auth_repository_impl.dart';
@@ -11,21 +12,29 @@ class StartUpPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final tabType = ref.watch(tabTypeProvider);
+    final isFirstLaunch =
+        ref.watch(sharedPreferencesServiceProvider).getIsFirstLaunch();
+    if (isFirstLaunch) {
+      ref
+          .read(sharedPreferencesServiceProvider)
+          .setIsFirstLaunch(isFirstLaunch: false);
+    }
 
     return Scaffold(
       resizeToAvoidBottomInset: tabType.index == 0 ? false : null,
       body: ref.watch(authUserProvider).when(
-            data: (data) => data != null
-                ? Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      const MainPage(),
-                      tabType.index == 0
-                          ? const FloatSearchBar()
-                          : const SizedBox(),
-                    ],
-                  )
-                : const SignInPage(),
+            data: (data) =>
+                data != null || !isFirstLaunch || !isFirstLaunch && data == null
+                    ? Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          MainPage(isAuthenticated: data != null),
+                          tabType.index == 0
+                              ? const FloatSearchBar()
+                              : const SizedBox(),
+                        ],
+                      )
+                    : const SignInPage(),
             error: (error, stackTrace) => ErrorPage(
               error: error,
               onTapReload: () => ref.invalidate(authUserProvider),
