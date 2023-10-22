f0a754b127876585445dda5eb1d026c4cd3eee5f
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-22 18:24:30 +0900

    ログインページをリファクタリングし、バリデーションを追加

--- lib/pages/auth/sign_in_page.dart
+++ lib/pages/auth/sign_in_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:search_roof_top_app/features/auth/sign_in.dart';
+import 'package:search_roof_top_app/pages/auth/components/auth_components.dart';
 import 'package:search_roof_top_app/pages/auth/send_reset_password_email_page.dart';
 import 'package:search_roof_top_app/pages/auth/sign_up_page.dart';
 import 'package:search_roof_top_app/pages/home/main_page.dart';
@@ -23,85 +24,119 @@ class SignInPage extends HookConsumerWidget {
     final passwordController = useTextEditingController();
     final passwordFocusNode = useFocusNode();
     final signIn = ref.read(signInControllerProvider.notifier).signInProvider;
+    final obscurePassword = useToggle(true);
+    final formKey = useFormStateKey();
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ログイン'),
-        elevation: 0,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: CommonTextField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              onFieldSubmitted: (_) =>
-                  FocusScope.of(context).requestFocus(passwordFocusNode),
-              textInputAction: TextInputAction.next,
-              labelText: 'メールアドレス',
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: CommonTextField(
-              controller: passwordController,
-              keyboardType: TextInputType.visiblePassword,
-              focusNode: passwordFocusNode,
-              textInputAction: TextInputAction.done,
-              labelText: 'パスワード',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              await ref.read(signIn).call(
-                    email: emailController.text,
-                    password: passwordController.text,
-                    onSuccess: () async {
-                      ScaffoldMessengerService.showSuccessSnackBar(
-                        context,
-                        'ログインしました!',
-                      );
-                      await Navigator.pushAndRemoveUntil(
-                        context,
-                        MainPage.route(isAuthenticated: true),
-                        (route) => false,
-                      );
+      appBar: const HomeAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'ログイン',
+                style: AppTextStyle.authPageTitle,
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: CommonTextField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      onFieldSubmitted: (_) => FocusScope.of(context)
+                          .requestFocus(passwordFocusNode),
+                      validator: Validator.email,
+                      textInputAction: TextInputAction.next,
+                      labelText: 'メールアドレス',
+                    ),
+                  ),
+                  CommonTextField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    focusNode: passwordFocusNode,
+                    textInputAction: TextInputAction.done,
+                    validator: Validator.password,
+                    obscureText: obscurePassword.value,
+                    maxLines: 1,
+                    labelText: 'パスワード',
+                    suffixIcon: PasswordVisibilityIcon(
+                      visibility: obscurePassword.value,
+                      onPressed: obscurePassword.toggle,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      child: const Text(
+                        'パスワードをお忘れの方',
+                        style: AppTextStyle.underline,
+                      ),
+                      onPressed: () async {
+                        await Navigator.push(
+                          context,
+                          SendResetPasswordEmailPage.route(),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  CommonButton(
+                    onPressed: () async {
+                      if (formKey.currentState!.validate()) {
+                        await ref.read(signIn).call(
+                              email: emailController.text,
+                              password: passwordController.text,
+                              onSuccess: () async {
+                                ScaffoldMessengerService.showSuccessSnackBar(
+                                  context,
+                                  'ログインしました!',
+                                );
+                                await Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MainPage.route(isAuthenticated: true),
+                                  (route) => false,
+                                );
+                              },
+                            );
+                      }
                     },
-                  );
-            },
-            child: const Text('ログイン'),
+                    text: 'ログイン',
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: CommonButton(
+                      onPressed: () async {
+                        await Navigator.push(
+                          context,
+                          SignUpPage.route(),
+                        );
+                      },
+                      color: ColorName.white,
+                      text: '新規登録',
+                    ),
+                  ),
+                ],
+              ),
+              // ElevatedButton(
+              //   child: const Text('スキップ'),
+              //   onPressed: () async {
+              //     await Navigator.pushAndRemoveUntil(
+              //       context,
+              //       MainPage.route(),
+              //       (route) => false,
+              //     );
+              //   },
+              // ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () async {
-              await Navigator.push(
-                context,
-                SignUpPage.route(),
-              );
-            },
-            child: const Text('新規登録'),
-          ),
-          ElevatedButton(
-            child: const Text('パスワードリセット'),
-            onPressed: () async {
-              await Navigator.push(
-                context,
-                SendResetPasswordEmailPage.route(),
-              );
-            },
-          ),
-          ElevatedButton(
-            child: const Text('スキップ'),
-            onPressed: () async {
-              await Navigator.pushAndRemoveUntil(
-                context,
-                MainPage.route(),
-                (route) => false,
-              );
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
