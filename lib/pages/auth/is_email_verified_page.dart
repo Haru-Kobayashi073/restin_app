79aa2015ed984eb12da755d359d3680b12864c5d
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-26 16:52:42 +0900

    ページを分けず、条件分岐で対応

--- lib/pages/auth/is_email_verified_page.dart
+++ lib/pages/auth/is_email_verified_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:search_roof_top_app/features/auth/auth.dart';
-import 'package:search_roof_top_app/pages/auth/complete_check_page.dart';
+import 'package:search_roof_top_app/pages/auth/components/auth_components.dart';
+import 'package:search_roof_top_app/pages/auth/entry_user_information_page.dart';
 import 'package:search_roof_top_app/utils/utils.dart';
 import 'package:search_roof_top_app/widgets/widgets.dart';
 
-class IsEmailVerifiedPage extends StatefulHookConsumerWidget {
+class IsEmailVerifiedPage extends HookConsumerWidget {
   const IsEmailVerifiedPage({super.key});
 
   static Route<dynamic> route() {
@@ -15,104 +17,85 @@ class IsEmailVerifiedPage extends StatefulHookConsumerWidget {
   }
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _IsEmailVerifiedPageState();
-}
-
-class _IsEmailVerifiedPageState extends ConsumerState<IsEmailVerifiedPage>
-    with WidgetsBindingObserver {
-  bool isEmailVerifiedState = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed && isEmailVerifiedState) {
-      Navigator.pushAndRemoveUntil(
-        context,
-        CompleteCheckPage.route(),
-        (_) => false,
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isEmailVerified = useState(false);
     WidgetsBinding.instance.addPostFrameCallback((_) {
       ref.read(emailVerificationTimerProvider).call(
         onSuccess: () {
-          if (!mounted) {
-            return;
-          }
-          setState(() {
-            isEmailVerifiedState = true;
-          });
+          isEmailVerified.value = true;
         },
       );
     });
-
-    return Scaffold(
-      appBar: const HomeAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  '送信が完了しました',
-                  style: AppTextStyle.authPageTitle,
+    return isEmailVerified.value
+        ? AuthPageWrapper(
+            canPop: false,
+            hasTitle: false,
+            authContent: AuthContent.emailVerification,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 32),
+                child: SvgPicture.asset(
+                  Assets.icons.checkCircle,
+                  width: context.deviceWidth * 0.2,
                 ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8),
-                  child: Text(
-                    'メールの確認が完了するとページが移動します!',
-                    style: AppTextStyle.bold,
+              ),
+              const Text(
+                '新規登録が完了しました',
+                style: AppTextStyle.authPageTitle,
+              ),
+              const SizedBox(height: 32),
+              CommonButton(
+                onPressed: () => Navigator.pushAndRemoveUntil(
+                  context,
+                  EntryUserInformationPage.route(),
+                  (_) => false,
+                ),
+                text: '情報入力画面へ',
+              )
+            ],
+          )
+        : AuthPageWrapper(
+            authContent: AuthContent.emailVerification,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: Text(
+                      'メールの確認が完了するとページが移動します!',
+                      style: AppTextStyle.bold,
+                    ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'ご入力いただいたメールアドレスに確認メールをお送りしましたので、ご確認ください。',
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      'ご入力いただいたメールアドレスに確認メールをお送りしましたので、ご確認ください。',
+                      style: AppTextStyle.greyText,
+                    ),
+                  ),
+                  const Text(
+                    '※ メールが届かない場合は',
                     style: AppTextStyle.greyText,
                   ),
-                ),
-                const Text(
-                  '※ メールが届かない場合は',
-                  style: AppTextStyle.greyText,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Column(
-                    children: precautionsForEmailVerified
-                        .map(
-                          (text) => ItemizedTextRow(text: text),
-                        )
-                        .toList(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: precautionsForEmailVerified
+                          .map(
+                            (text) => ItemizedTextRow(text: text),
+                          )
+                          .toList(),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Container(
-              alignment: Alignment.center,
-              child: const CircularProgressIndicator(),
-            ),
-            SizedBox(height: context.deviceHeight * 0.06)
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
+                ],
+              ),
+              SizedBox(height: context.deviceHeight * 0.1),
+              Container(
+                alignment: Alignment.center,
+                child: const CircularProgressIndicator(),
+              ),
+            ],
+          );
   }
 }
