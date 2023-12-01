5d6a929157b5c27969074c5c8786f6bf3e44e0bb
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-12-01 11:21:05 +0900

    repositoryにGeoCoding APIを叩く処理を追加

--- lib/repositories/marker/marker_repository_impl.dart
+++ lib/repositories/marker/marker_repository_impl.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: avoid_dynamic_calls
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:search_roof_top_app/models/comment.dart';
@@ -13,10 +17,12 @@ final markerRepositoryImplProvider =
 class MarkerRepositoryImpl implements MarkerRepository {
   MarkerRepositoryImpl(ProviderRef<MarkerRepository> ref)
       : _auth = ref.read(authProvider),
-        _firestore = ref.read(firestoreProvider);
+        _firestore = ref.read(firestoreProvider),
+        _dio = ref.read(dioProvider);
 
   final FirebaseAuth _auth;
   final FirebaseFirestore _firestore;
+  final Dio _dio;
 
   @override
   User? get currentUser => _auth.currentUser;
@@ -150,6 +156,15 @@ class MarkerRepositoryImpl implements MarkerRepository {
 
   @override
   Future<String> fetchMarkerAddress({required LatLng latLng}) async {
-  return '';
+    final response = await _dio.get<dynamic>(
+      'https://maps.googleapis.com/maps/api/geocode/json',
+      queryParameters: {
+        'latlng': '${latLng.latitude},${latLng.longitude}',
+        'key': dotenv.get('GOOGLE_MAPS_API_KEY_FOR_IOS'),
+        'language': 'ja',
+      },
+    );
+    final decoded = response.data;
+    return decoded['results'][0]['formatted_address'].toString();
   }
 }
